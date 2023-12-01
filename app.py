ecbb5ffb8ccbe9502d80e376b6e09fcdc386c255
charmelcode <152369374+charmelcode@users.noreply.github.com>
2023-12-01 16:24:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write('Hello World')
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage",page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+st.subheader("Hi, I am Charmel :wave:")
+st.title("A student from Surigao del Norte State University")
+st.write("I am passionate to learn about python. ")
